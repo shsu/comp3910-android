93014e46984cc429a4ea930e261eb189096fc6f4
Joseph <jesusnj@gmail.com>
2013-11-27 23:01:16 -0800

    Added Welcome Screen

--- gen/ca/bcit/turnip/R.java
+++ gen/ca/bcit/turnip/R.java
@@ -24,34 +24,41 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
+        public static final int action_settings=0x7f080016;
         public static final int button1=0x7f080004;
         public static final int button2=0x7f080005;
         public static final int button_register=0x7f080010;
+        public static final int button_start_next_quiz=0x7f080014;
         public static final int editText1=0x7f080003;
-        public static final int editText_firstName=0x7f08000d;
-        public static final int editText_lastName=0x7f08000e;
-        public static final int editText_password=0x7f080009;
-        public static final int editText_studentId=0x7f08000f;
+        public static final int editText_firstName=0x7f08000c;
+        public static final int editText_lastName=0x7f08000d;
+        public static final int editText_password=0x7f080008;
+        public static final int editText_studentId=0x7f08000e;
         public static final int editText_username=0x7f080001;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
         public static final int textView_firstName=0x7f08000a;
-        public static final int textView_lastName=0x7f08000c;
+        public static final int textView_lastName=0x7f08000b;
         public static final int textView_new_user_registration=0x7f080006;
-        public static final int textView_password=0x7f080008;
-        public static final int textView_studentNumber=0x7f08000b;
+        public static final int textView_password=0x7f080009;
+        public static final int textView_quiz_results=0x7f080013;
+        public static final int textView_quizzes_taken=0x7f080012;
+        public static final int textView_studentId=0x7f08000f;
+        public static final int textView_user_average_score=0x7f080015;
         public static final int textView_username=0x7f080007;
+        public static final int textView_welcome=0x7f080011;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_new_user=0x7f030001;
-        public static final int activity_welcome=0x7f030002;
+        public static final int activity_register=0x7f030002;
+        public static final int activity_welcome=0x7f030003;
     }
     public static final class menu {
         public static final int first=0x7f070000;
         public static final int new_user=0x7f070001;
-        public static final int second=0x7f070002;
+        public static final int register=0x7f070002;
+        public static final int second=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -70,10 +77,11 @@ public final class R {
         public static final int register=0x7f050018;
         public static final int second=0x7f050004;
         public static final int start_next_quiz=0x7f050013;
-        public static final int studentNumber=0x7f05000b;
+        public static final int studentId=0x7f05000b;
         public static final int title_activity_newUser=0x7f05000e;
         public static final int title_activity_new_user=0x7f050005;
         public static final int title_activity_quiz=0x7f050016;
+        public static final int title_activity_register=0x7f050019;
         public static final int title_activity_second=0x7f050003;
         public static final int title_activity_welcome=0x7f05000f;
         public static final int user_average_score=0x7f050015;
--- src/ca/bcit/turnip/NewUserActivity.java
+++ src/ca/bcit/turnip/NewUserActivity.java
@@ -2,10 +2,27 @@ package ca.bcit.turnip;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class NewUserActivity extends Activity {
 
+	EditText et_username;
+	EditText et_password;
+	EditText et_firstName;
+	EditText et_lastName;
+	EditText et_studentId;
+	
+	
+	String username;
+	String password;
+	String firstName;
+	String lastName;
+	String studentId;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +34,19 @@ public class NewUserActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.new_user, menu);
 		return true;
+	}
+	
+	public void sendRegister(View view) {
+		et_username = (EditText) findViewById(R.id.editText_username);
+		et_password = (EditText) findViewById(R.id.editText_password);
+		et_firstName = (EditText) findViewById(R.id.editText_firstName);
+		et_lastName = (EditText) findViewById(R.id.editText_lastName);
+		et_studentId = (EditText) findViewById(R.id.editText_studentId);
+		
+//		Intent intent = new Intent(this, RegisterActivity.class);
+		
+		// Registration
+		
 	}
 
 }
