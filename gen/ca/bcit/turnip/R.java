92e893e3c83e73d026e4c007d6c34d96880fafe1
Joseph <jesusnj@gmail.com>
2013-11-30 14:57:55 -0800

    git ignored

--- gen/ca/bcit/turnip/R.java
+++ gen/ca/bcit/turnip/R.java
@@ -27,14 +27,14 @@ public final class R {
         public static final int RadioButton_c=0x7f080018;
         public static final int RadioButton_d=0x7f080019;
         public static final int TextView_confirm_password=0x7f080011;
-        public static final int action_settings=0x7f080024;
+        public static final int action_settings=0x7f080026;
         public static final int button1=0x7f080004;
         public static final int button2=0x7f080005;
         public static final int button_login=0x7f08001c;
-        public static final int button_logout=0x7f080023;
+        public static final int button_logout=0x7f080025;
         public static final int button_register=0x7f080010;
         public static final int button_return=0x7f08001d;
-        public static final int button_start_next_quiz=0x7f080021;
+        public static final int button_start_next_quiz=0x7f080023;
         public static final int button_submit=0x7f08001a;
         public static final int editText1=0x7f080003;
         public static final int editText_confirm_password=0x7f080012;
@@ -54,13 +54,15 @@ public final class R {
         public static final int textView_password=0x7f080009;
         public static final int textView_q1=0x7f080014;
         public static final int textView_quiz_number=0x7f080013;
-        public static final int textView_quiz_results=0x7f080020;
-        public static final int textView_quizzes_taken=0x7f08001f;
+        public static final int textView_quiz_results=0x7f080022;
+        public static final int textView_quizzes_taken=0x7f080021;
         public static final int textView_register_msg=0x7f08001b;
         public static final int textView_studentId=0x7f08000f;
-        public static final int textView_user_average_score=0x7f080022;
+        public static final int textView_user_average_score=0x7f080024;
         public static final int textView_username=0x7f080007;
-        public static final int textView_welcome=0x7f08001e;
+        public static final int textView_welcome=0x7f080020;
+        public static final int textView_your_score_is=0x7f08001e;
+        public static final int textdView_quiz_score=0x7f08001f;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -85,14 +87,14 @@ public final class R {
         public static final int average_score=0x7f050014;
         public static final int b=0x7f05001f;
         public static final int c=0x7f050020;
-        public static final int confirm_password=0x7f050027;
+        public static final int confirm_password=0x7f050028;
         public static final int d=0x7f050021;
         public static final int enter=0x7f050006;
         public static final int firstName=0x7f05000c;
         public static final int hello_world=0x7f050002;
         public static final int lastName=0x7f05000d;
         public static final int login=0x7f05001b;
-        public static final int logout=0x7f050026;
+        public static final int logout=0x7f050027;
         public static final int new_user=0x7f050009;
         public static final int new_user_registration=0x7f05000a;
         public static final int password=0x7f050008;
@@ -100,10 +102,11 @@ public final class R {
         public static final int quizNumber=0x7f05001c;
         public static final int quizQ1=0x7f05001d;
         public static final int quiz_results=0x7f050012;
+        public static final int quiz_score=0x7f050025;
         public static final int quizzes_taken=0x7f050010;
         public static final int register=0x7f050018;
         public static final int register_success=0x7f05001a;
-        public static final int return_welcome=0x7f050025;
+        public static final int return_welcome=0x7f050026;
         public static final int second=0x7f050004;
         public static final int start_next_quiz=0x7f050013;
         public static final int studentId=0x7f05000b;
@@ -118,7 +121,7 @@ public final class R {
         public static final int user_average_score=0x7f050015;
         public static final int username=0x7f050007;
         public static final int welcome=0x7f050011;
-        public static final int your_score=0x7f050024;
+        public static final int your_score_is=0x7f050024;
     }
     public static final class style {
         /** 
