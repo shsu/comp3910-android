ce1312699ce5cca1c3212c403b0d18ebac998d14
Steven Hsu <admin@stevenhsu.ca>
2013-12-03 15:32:55 -0800

    Use RHCloud

--- src/ca/bcit/turnip/config/Config_RestServer.java
+++ src/ca/bcit/turnip/config/Config_RestServer.java
@@ -2,7 +2,7 @@ package ca.bcit.turnip.config;
 
 public final class Config_RestServer {
 
-	public static final String REST_SERVER_URL = "http://10.0.2.2:8080/a3-server-jhou-shsu/";
+	public static final String REST_SERVER_URL = "http://quiz-stevenhsu.rhcloud.com/";
 
 	private Config_RestServer() {
 
